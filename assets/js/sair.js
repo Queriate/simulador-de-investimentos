f48eac14342e0d03a774ff030add47b081945cf4
queriate <queriatejobs@gmail.com>
2024-10-29 13:44:14 -0300

    melhorando leitura do codigo

--- assets/js/sair.js
+++ assets/js/sair.js
@@ -1,3 +1,5 @@
+
+//saída de iniciar para conteiner 
 document.querySelector('.simular').addEventListener('click', function() {
     const inicial = document.querySelector('.inicial');
     const container = document.querySelector('.container');
@@ -6,3 +8,11 @@ document.querySelector('.simular').addEventListener('click', function() {
     container.classList.add('visible');
   });
   
+  //saida de conteiner para resoltados 
+  document.querySelector('.rusoltados').addEventListener('click', function() {
+    const inicial = document.querySelector('.conteiner');
+    const container = document.querySelector('.resultados');
+    
+    inicial.classList.add('hidden');
+    container.classList.add('visible');
+  });
